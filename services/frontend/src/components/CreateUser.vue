574e76a3ae796db6f1e1f11dfb8f4bb1f056cf5d
Thomas Menchi <thomas.menchi@edu.esiee.fr>
2021-11-04 19:42:56 +0100

    fixing creation of users

--- services/frontend/src/components/CreateUser.vue
+++ services/frontend/src/components/CreateUser.vue
@@ -2,14 +2,14 @@
   <form>
       <div class="form-group">
         <label for="inputEmail">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <input v-model="form.email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <input v-model="form.password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="button" class="btn btn-primary" v-on:click="submit">Submit</button>
     </form>
 </template>
 

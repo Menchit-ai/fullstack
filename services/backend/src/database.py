abe38aec1be58b3dcced7ab0316abe691d86ffa2
Menchi <65895366+Menchit-ai@users.noreply.github.com>
2021-11-06 15:58:38 +0100

    Update database.py

--- services/backend/src/database.py
+++ services/backend/src/database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# POSTGRES_USER = os.environ["POSTGRES_USER"]
-# POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
-# POSTGRES_DB = os.environ["POSTGRES_DB"]
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+POSTGRES_DB = os.environ["POSTGRES_DB"]
 
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_DB}/postgres'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/postgres'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_DB}/postgres'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
